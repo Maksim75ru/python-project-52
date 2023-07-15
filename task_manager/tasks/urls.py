002e48c790a39607958d9003efa0a746bf667d09
Maksim Lesnikov <maximborzya75@gmail.com>
2023-07-16 00:26:51 +0700

    step_7

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -3,9 +3,9 @@ from task_manager.tasks import views
 
 
 urlpatterns = [
-    path('', views.TasksListView.as_view(), name='tasks_list'),
-    path('create/', views.CreateTask.as_view(), name='task_create'),
-    path('<int:pk>/update/', views.UpdateTask.as_view(), name='task_update'),
-    path('<int:pk>/delete/', views.DeleteTask.as_view(), name='task_delete'),
-    path('<int:pk>/', views.TaskDetailsView.as_view(), name='task_details'),
+    path("", views.TasksListView.as_view(), name="tasks_list"),
+    path("create/", views.CreateTask.as_view(), name="task_create"),
+    path("<int:pk>/update/", views.UpdateTask.as_view(), name="task_update"),
+    path("<int:pk>/delete/", views.DeleteTask.as_view(), name="task_delete"),
+    path("<int:pk>/", views.TaskDetailsView.as_view(), name="task_details"),
 ]

fc22a4eb6b2e19688b8dc805a2c4824d9dd30293
Maksim Lesnikov <maximborzya75@gmail.com>
2023-07-16 15:18:32 +0700

    step_8

--- task_manager/views.py
+++ task_manager/views.py
@@ -5,10 +5,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "index.html")
+    a = None
+    a.hello() # Creating an error with an invalid line of code
+    return HttpResponse("Hello, world. You're at the pollapp index.")
 
 
 class HomePageView(TemplateView):

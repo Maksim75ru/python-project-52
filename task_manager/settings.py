fdde660a31874a03b3a946f45076ab801e210e62
Maksim Lesnikov <maximborzya75@gmail.com>
2023-07-18 21:36:40 +0700

    after_review

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -127,7 +127,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "example.storages.ExtendedFileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "example.storages.ExtendedS3Storage",
+    },
+}
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type

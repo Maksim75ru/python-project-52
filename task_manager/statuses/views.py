82a167085c774bb474b8cc4910d1e372a504b05e
Maksim Lesnikov <maximborzya75@gmail.com>
2023-07-28 21:54:33 +0700

    after third review - handle with status 500 (change to 400)

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,4 @@
+from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
@@ -5,6 +6,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.utils.translation import gettext as _
 from django.urls import reverse_lazy
+from django.http import HttpResponseBadRequest
+
 from task_manager.statuses.models import Status
 from task_manager.mixins import LoginRequiredMixin
 
@@ -37,15 +40,21 @@ class UpdateStatus(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     success_message = _("Status changed")
 
 
-class DeleteStatus(LoginRequiredMixin, DeleteView):
+class DeleteStatus(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Status
     template_name = "statuses/delete_status.html"
     success_url = reverse_lazy("statuses_list")
+    success_message = _("Status deleted")
+    deny_delete_message = _("It is not possible to delete a status because it is in use")
+    deny_delete_url = reverse_lazy('labels_list')
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         if not self.object:
             messages.warning(request, _("Can't delete status"))
             return redirect(self.get_success_url())
-        messages.success(request, _("Status deleted"))
-        return super().post(request, *args, **kwargs)
+
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            return HttpResponseBadRequest(self.deny_delete_message)
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,3 +1,5 @@
+from django.db.models import ProtectedError
+from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
@@ -5,6 +7,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.utils.translation import gettext as _
 from django.urls import reverse_lazy
+
 from task_manager.mixins import LoginRequiredMixin, UserRequiredMixin
 from task_manager.users.models import UsersModel
 from task_manager.users import forms
@@ -43,15 +46,21 @@ class UserUpdateView(
     permission_denied_message = _("You have no rights to change another user.")
 
 
-class UserDeleteView(LoginRequiredMixin, UserRequiredMixin, DeleteView):
+class UserDeleteView(LoginRequiredMixin, UserRequiredMixin, SuccessMessageMixin, DeleteView):
     model = UsersModel
     template_name = "users/delete.html"
     success_url = reverse_lazy("users_list")
+    success_message = _("User is successfully deleted")
+    deny_delete_message = _('Unable to delete a user because he is being used')
+    deny_delete_url = reverse_lazy('users_list')
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         if not self.object:
             messages.warning(request, _("Can't delete user"))
             return redirect(self.get_success_url())
-        messages.success(request, _("User is successfully deleted"))
-        return super().post(request, *args, **kwargs)
+
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            return HttpResponseBadRequest(self.deny_delete_message)
